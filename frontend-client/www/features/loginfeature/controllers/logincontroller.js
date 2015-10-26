e4c2bcd66563ae74fd4c8dca959c6eb8c842317b
Gabriel Humphrey <gabe2390@gmail.com>
2015-10-26 15:25:53 -0400

    d

--- frontend-client/www/features/loginfeature/controllers/logincontroller.js
+++ frontend-client/www/features/loginfeature/controllers/logincontroller.js
@@ -3,12 +3,19 @@ angular.module('worldquest.controllers', [])
 .controller('logincontroller', function($scope, loginservice){
 
 		var team= {};
-	 	
+	 	$verifiedTeam={};
+
 	 	$scope.sendForVerification= function(){
 			team.name=$scope.name;
 			team.password= $scope.password;
 			team.url = "?name=" + $scope.name + "&password=" + $scope.password;
-			console.log(team);
-			loginservice.sendForVerification(team);
+			loginservice.sendForVerification(team.url);
 	}
+
+		 $scope.$watch(function(){return loginservice.getVerified();}, function(newVal, oldVal) {
+		     if (typeof newVal !== 'undefined') {
+		        $scope.verifiedTeam = newVal;
+		        console.log(newVal)	;
+		        }
+      });
 })
--- frontend-client/www/features/loginfeature/services/loginservice.js
+++ frontend-client/www/features/loginfeature/services/loginservice.js
@@ -2,23 +2,24 @@ angular.module('worldquest.services', [])
 
 .factory('loginservice', function($http){
 	verifiedTeam= {};
-	url = 'http://127.0.0.1:8080/team/auth';
-
 
 	function sendForVerification(loginInfo){	
 		console.log(loginInfo);
 		$http({
 			method: 'GET',
-			url: url + loginInfo.url,
+			url: 'http://127.0.0.1:8080/team/auth' + loginInfo,
 		}).then(function successCallback(output){
-			console.log(output);
 			verifiedTeam = output.data;
-			console.log(verifiedTeam);
 			return verifiedTeam;
 		})
 	}
+
+	function getVerified(){
+		return verifiedTeam;
+	}
 		
 	return {
-		sendForVerification: sendForVerification
+		sendForVerification: sendForVerification,
+		getVerified : getVerified
 	};
 });
