481971bc0445dc051886ff29a813ed7c166a0200
Gregory Furlong <gnfurlong@gmail.com>
2015-10-26 15:49:45 -0400

    More database fixes.

--- backend/.provision/database_fixture.sql
+++ backend/.provision/database_fixture.sql
@@ -1,3 +1,3 @@
 INSERT INTO team(name, hash) VALUES("user", "password");
 INSERT INTO question(problem, answer_a, answer_b, answer_c, answer_d, correct_index) VALUES("What city is the host of the 2022 Winter Olympics?", "Tokyo", "Beijing", "Pyeongyang", "Almaty", 1);
-INSERT INTO answer(team, question, answer_index, time_answered) VALUES(1,1,1,1);
+INSERT INTO question(problem, answer_a, answer_b, answer_c, answer_d, correct_index) VALUES("What city are we in?", "Wilmington", "DC", "Baltimore", "Annapolis", 0);
--- backend/src/main/java/io/zipcoder/production/worldquest/controllers/AnswerDecisionEngineController.java
+++ backend/src/main/java/io/zipcoder/production/worldquest/controllers/AnswerDecisionEngineController.java
@@ -2,6 +2,8 @@ package io.zipcoder.production.worldquest.controllers;
 
 import io.zipcoder.production.worldquest.models.Answer;
 import io.zipcoder.production.worldquest.models.Question;
+import io.zipcoder.production.worldquest.models.QuestionDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,13 +14,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class AnswerDecisionEngineController {
+    private static int nextId = 0;
 
+    @Autowired
+    private QuestionDAO questionDAO;
     String[] answers = new String[]{"Tokyo","Beijing","Pyeongchang","Almaty"};
     Question question = new Question("Which city is the host of 2022 Winter Olympic Games?", answers, 1);
 
     @RequestMapping(value = "/question", method = RequestMethod.GET)
     @ResponseBody
     public Question getQuestion(){
+        Question question = questionDAO.findOne(++nextId);
+        if(question == null) {
+            nextId = 1;
+            question = questionDAO.findOne(nextId);
+        }
+        question.buildAnswerArray();
         return question;
     }
 
@@ -26,6 +37,8 @@ public class AnswerDecisionEngineController {
     @ResponseBody
     public boolean sendAnswer(Integer questionID, Integer teamID, Integer answerIndex){
         Answer answer = new Answer(questionID, teamID, answerIndex);
+        Question question = questionDAO.findOne(questionID);
+
         if(question.getCorrectIndex()==answerIndex){
             return true;
         }
