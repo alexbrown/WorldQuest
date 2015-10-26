88f77244c3888bc10b3c866707452aeaa561a460
Gregory Furlong <gnfurlong@gmail.com>
2015-10-26 10:43:43 -0400

    Initial commit.

---
+++ backend/src/main/java/io/zipcoder/production/worldquest/WorldQuestBackendApplication.java
@@ -0,0 +1,12 @@
+package io.zipcoder.production.worldquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldQuestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldQuestBackendApplication.class, args);
+    }
+}
