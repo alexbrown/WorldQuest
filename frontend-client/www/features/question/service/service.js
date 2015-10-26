b3292868161ea5ab3092ead504ec0f76174874d0
alexbrown <abrown@zipcode7s-MacBook-Pro.local>
2015-10-26 11:55:01 -0400

    Worked on service

--- frontend-client/www/features/question/service/service.js
+++ frontend-client/www/features/question/service/service.js
@@ -10,6 +10,16 @@ angular.module('WorldQuest.question.services', [])
 		question = response.data;
 	});
 
+	function sendAnswer(awnser){
+		$http({
+			method: 'POST',
+			url: 'http://localhost:8080/answer',
+			data: awnser
+		}).then(function successCallback(output){
+			return output
+		})
+	}
+
 	return{
 		all:function(){
 			return question;
