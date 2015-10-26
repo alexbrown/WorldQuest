c09214b6eb0c24cd0f3218585608ec74d82b2077
Gabriel Humphrey <gabe2390@gmail.com>
2015-10-26 12:57:07 -0400

    Set up http request to send login info to server

--- frontend-client/www/features/loginfeature/controllers/logincontroller.js
+++ frontend-client/www/features/loginfeature/controllers/logincontroller.js
@@ -2,5 +2,14 @@ angular.module('worldquest.controllers', [])
 
 .controller('logincontroller', function($scope, loginservice){
 
-	
+		var team= {};
+	 	
+	 	$scope.sendForVerification= function(){
+			team.name=$scope.name;
+			team.password= $scope.password;
+
+			loginservice.sendForVerification(team);
+	}
+
+
 })
--- frontend-client/www/features/loginfeature/services/loginservice.js
+++ frontend-client/www/features/loginfeature/services/loginservice.js
@@ -1,5 +1,33 @@
 angular.module('worldquest.services', [])
 
-.factory('loginservice', function($scope, $http){
+.factory('loginservice', function($http){
+	verifiedTeam= {};
+	url = 'http://127.0.0.1:8080/team/auth';
 
-})
+
+	function sendForVerification(loginInfo){
+		$http({
+			method: 'POST',
+			url: url,
+			data: loginInfo
+		}).then(function successCallback(output){
+			verifiedTeam = output.data;
+			return verifiedTeam;
+		})
+	}
+
+	function getResponse(){
+		return response;
+	}
+		
+	return {
+		sendForVerification: sendForVerification,
+		getResponse: getResponse
+	};
+	
+
+
+	return {
+		verify : verify
+	}
+});
