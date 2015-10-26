7c1d2fbf2632f563cf37cc9d883238a60514b574
SparksRichard <rsparks@Zips-MacBook-Pro-47.local>
2015-10-26 14:17:16 -0400

    Updated classes for database, changed Question's answer array functionality

--- backend/src/main/java/io/zipcoder/production/worldquest/models/Question.java
+++ backend/src/main/java/io/zipcoder/production/worldquest/models/Question.java
@@ -13,8 +13,21 @@ public class Question {
     @NotNull
     private String problem;
 
-    @NotNull
     private String[] possibleAns;
+
+    @NotNull
+    private String answer_a;
+
+    @NotNull
+    private String answer_b;
+
+    @NotNull
+    private String answer_c;
+
+
+    @NotNull
+    private String answer_d;
+
 
     @NotNull
     private int correctIndex;
@@ -25,11 +38,54 @@ public class Question {
 
     public Question(){};
 
-    public Question(String problem, String[] possibleAns, int correctIndex) {
+    public Question(String problem, String answer_a, String answer_b, String answer_c, String answer_d, int correctIndex) {
         this.problem = problem;
-        this.possibleAns = possibleAns;
+        this.answer_a = answer_a;
+        this.answer_b = answer_b;
+        this.answer_c = answer_c;
+        this.answer_d = answer_d;
         this.correctIndex = correctIndex;
+        possibleAns = new String[]{answer_a, answer_b, answer_c, answer_d};
     }
+
+    public void buildAnswerArray(){
+        possibleAns = new String[]{answer_a, answer_b, answer_c, answer_d};
+
+    }
+
+    public String getAnswer_a() {
+        return answer_a;
+    }
+
+    public void setAnswer_a(String answer_a) {
+        this.answer_a = answer_a;
+    }
+
+    public String getAnswer_b() {
+        return answer_b;
+    }
+
+    public void setAnswer_b(String answer_b) {
+        this.answer_b = answer_b;
+    }
+
+    public String getAnswer_c() {
+        return answer_c;
+    }
+
+    public void setAnswer_c(String answer_c) {
+        this.answer_c = answer_c;
+    }
+
+    public String getAnswer_d() {
+        return answer_d;
+    }
+
+    public void setAnswer_d(String answer_d) {
+        this.answer_d = answer_d;
+    }
+
+
 
     public void setProblem(String problem) {
         this.problem = problem;
