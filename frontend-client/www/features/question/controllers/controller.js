bb0cf4de6cb1177d53cd1c1aa9bd2623b42b7cb1
SweetBroHellaCode <etmaron@gmail.com>
2015-10-26 13:25:50 -0400

    Added question and answer functionality

--- frontend-client/www/features/question/controllers/controller.js
+++ frontend-client/www/features/question/controllers/controller.js
@@ -2,10 +2,24 @@ var app = angular.module('WorldQuest.question.controller', []);
 
 app.controller('QuestionCtrl', function($scope, Questions){
 	$scope.questions = Questions.all();
+	$scope.questionID= Questions.all().id;
+	$scope.teamID = 12;
 
 	$scope.$watch(function(){return Questions.all();}, function(newVal, oldVal) {
 		if (typeof newVal !== 'undefined') {
         $scope.questions = newVal;
+        $scope.questionID = newVal.id;
         console.log(newVal);
     }});
+
+
+	$scope.selectedAnswer = function(index){
+		var answer = {
+			questionID: $scope.questionID,
+			teamID: $scope.teamID,
+			answerIndex: index
+		}
+		Questions.sendAnswer(answer);
+	}
+
 })
